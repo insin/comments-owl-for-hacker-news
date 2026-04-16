d483ec6df1351d5014a35616069409631159997a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2026-04-16 15:49:36 +1000

    Add Show/Hide Filtered Items menu item
    This is currently parked in a branch as it results in a sub-menu which is
    too big due to the exension name

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 import {get, set} from './storage.js'
 
 const TOGGLE_AI_ITEMS = 'toggle-ai-items'
+const TOGGLE_CUSTOM_ITEMS = 'toggle-custom-items'
 const TOGGLE_REPLY_LINKS = 'toggle-reply-links'
 
 const ITEM_PAGE_URL_PATTERNS = [
@@ -18,11 +19,17 @@ chrome.runtime.onInstalled.addListener(async (details) => {
   if (details.reason == 'update') {
     await chrome.contextMenus.removeAll()
   }
-  let {hideAiItems, hideReplyLinks} = await get(['hideAiItems', 'hideReplyLinks'])
+  let {hideAiItems, hideCustomItems, hideReplyLinks} = await get(['hideAiItems', 'hideCustomItems', 'hideReplyLinks'])
   chrome.contextMenus.create({
     id: TOGGLE_AI_ITEMS,
     contexts: ['page'],
     title: chrome.i18n.getMessage(hideAiItems ? 'showAiItemsMenuItem' : 'hideAiItemsMenuItem'),
+    documentUrlPatterns: ITEM_PAGE_URL_PATTERNS,
+  })
+  chrome.contextMenus.create({
+    id: TOGGLE_CUSTOM_ITEMS,
+    contexts: ['page'],
+    title: chrome.i18n.getMessage(hideCustomItems ? 'showCustomItemsMenuItem' : 'hideCustomItemsMenuItem'),
     documentUrlPatterns: ITEM_PAGE_URL_PATTERNS,
   })
   chrome.contextMenus.create({
@@ -38,6 +45,10 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
     let {hideAiItems} = await get('hideAiItems')
     await set({hideAiItems: !hideAiItems})
   }
+  if (info.menuItemId == TOGGLE_CUSTOM_ITEMS) {
+    let {hideCustomItems} = await get('hideCustomItems')
+    await set({hideCustomItems: !hideCustomItems})
+  }
   if (info.menuItemId == TOGGLE_REPLY_LINKS) {
     let {hideReplyLinks} = await get('hideReplyLinks')
     await set({hideReplyLinks: !hideReplyLinks})
@@ -48,6 +59,11 @@ chrome.storage.local.onChanged.addListener((changes) => {
   if (changes.hideAiItems) {
     chrome.contextMenus.update(TOGGLE_AI_ITEMS, {
       title: chrome.i18n.getMessage(changes.hideAiItems.newValue ? 'showAiItemsMenuItem' : 'hideAiItemsMenuItem'),
+    })
+  }
+  if (changes.hideCustomItems) {
+    chrome.contextMenus.update(TOGGLE_CUSTOM_ITEMS, {
+      title: chrome.i18n.getMessage(changes.hideCustomItems.newValue ? 'showCustomItemsMenuItem' : 'hideCustomItemsMenuItem'),
     })
   }
   if (changes.hideReplyLinks) {
