61a355f29f75118a28f354507d98933575e6e5fd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2026-04-17 11:42:01 +1000

    Update app with latest changes from Control Panel

--- safari/Shared (App)/Resources/Script.js
+++ safari/Shared (App)/Resources/Script.js
@@ -1,22 +1,11 @@
-function show(platform, enabled, useSettingsInsteadOfPreferences) {
+function show(platform, enabled) {
   document.body.classList.add(`platform-${platform}`)
-
-  if (useSettingsInsteadOfPreferences) {
-    document.querySelector('.platform-mac.state-on').innerText = 'Comments Owl for Hacker News’ extension is currently on. You can turn it off in the Extensions section of Safari Settings.'
-    document.querySelector('.platform-mac.state-off').innerText = 'Comments Owl for Hacker News’ extension is currently off. You can turn it on in the Extensions section of Safari Settings.'
-    document.querySelector('.platform-mac.state-unknown').innerText = 'You can turn on Comments Owl for Hacker News’ extension in the Extensions section of Safari Settings.'
-    document.querySelector('.open-preferences').innerText = 'Quit and Open Safari Settings…'
-  }
-
-  if (typeof enabled === 'boolean') {
+  if (typeof enabled == 'boolean') {
     document.body.classList.toggle('state-on', enabled)
     document.body.classList.toggle('state-off', !enabled)
-  } else {
-    document.body.classList.remove('state-on', 'state-off')
   }
-
-  if (platform === 'ios') {
-    document.querySelector('.open-preferences').innerText = 'Open Safari Extensions Preferences…'
+  else if (platform == 'mac') {
+    document.body.classList.add('state-unknown')
   }
 }
 
